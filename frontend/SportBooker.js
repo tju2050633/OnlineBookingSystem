cf6a79f512f1ad61c254464cc0d5e29c288d8bfb
ZHANGJiahuic <zjh57036660@gmail.com>
2024-03-31 20:00:14 +0800

    frontend

--- frontend/SportBooker.js
+++ frontend/SportBooker.js
@@ -14,5 +14,21 @@ function Gofacility() {
 }
 
 function Govenue() {
-
+  var SC = document.getElementById("Center");
+  var SC_option = SC.value;
+  var facility = document.getElementById("SC").children;
+  var venue = document.getElementById("SC_f").children;
+  for (var i = 0; i < venue.length; i++) {
+    var facility_option = facility[i].value
+    var facility_venue = venue[i].children
+    for (var j = 0; j < facility_venue.length; j++){
+      var question = facility_venue[j];
+      if (question.id === SC_option +" "+ facility_option) {
+        question.style.display = "block";
+      }
+      else {
+        question.style.display = "none";
+      }
+    }
+  }
 }
