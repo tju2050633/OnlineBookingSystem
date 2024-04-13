499846bd9fda5be8bb7eefd72ad5ff2b7a6234c3
Jialin Lu <1640889387@qq.com>
2024-04-13 19:56:50 +0800

    correct server ip

--- ApiTest/Membership.js
+++ ApiTest/Membership.js
@@ -1,4 +1,4 @@
-membershipBaseUrl = "http://10.12.13.162:8080/api/membership/"
+membershipBaseUrl = "http://34.92.41.227:8080/api/membership/"
 
 function getAllMembershipSales() {
     return $.ajax({
